87ff3ebba8651ec4294e4d5e1c803bd79899baab
EKT <efekagantelevi@gmail.com>
2023-05-02 11:10:53 +0300

    coinbox animation added

---
+++ Assets/Scripts/UICoinImage.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class UICoinImage : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        GameManager.Instance.OnCoinsChanged += Pulse;
+    }
+
+    private void Pulse(int coins)
+    {
+        animator.SetTrigger("Pulse");
+    }
+
+}
