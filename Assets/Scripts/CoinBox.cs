e67306e00f2354f05d74cb911f14a18c4f1ee8f4
EKT <efekagantelevi@gmail.com>
2023-05-02 10:25:54 +0300

    onlygetcoinbottom

--- Assets/Scripts/CoinBox.cs
+++ Assets/Scripts/CoinBox.cs
@@ -22,16 +22,31 @@ public class CoinBox : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (remainingCoins > 0 &&
-            collision.collider.GetComponent<PlayerMovementController>() != null)
+            WasHitByPlayer(collision) &&
+            WasHitFromBottomSide(collision))
         {
-            GameManager.Instance.AddCoin();
-            remainingCoins--;
-
-            if (remainingCoins <= 0)
+            if (collision.contacts[0].normal.y > 0.5) ;
             {
-                enabledSprite.enabled = false;
-                disabledSprite.enabled = true;
+
+                GameManager.Instance.AddCoin();
+                remainingCoins--;
+
+                if (remainingCoins <= 0)
+                {
+                    enabledSprite.enabled = false;
+                    disabledSprite.enabled = true;
+                }
             }
         }
+    }
+
+    private static bool WasHitByPlayer(Collision2D collision)
+    {
+        return collision.collider.GetComponent<PlayerMovementController>() != null;
+    }
+
+    private static bool WasHitFromBottomSide(Collision2D collision)
+    {
+        return collision.contacts[0].normal.y > 0.5;
     }
 }
